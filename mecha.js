9f33fe33017acabc15df1fc4b1cb9416897dae5b
Luca Wellmeier <lucwellm@gmail.com>
2022-09-25 17:51:36 +0200

    translate into simple entity-component system

--- mecha.js
+++ mecha.js
@@ -1,3 +1,97 @@
+class Entity {
+    constructor(game) {
+        this.game = game;
+        this.components = {}
+    }
+
+    update() {
+        Object.values(this.components).forEach(comp => {
+            comp.update();
+        });
+    }
+    draw() {
+        Object.values(this.components).forEach(comp => {
+            comp.draw();
+        });
+    }
+
+    add(compObj) {
+        compObj.game = this.game;
+        compObj.entity = this;
+        this.components[compObj.name] = compObj;
+    }
+    get(comp) {
+        return this.components[comp];
+    }
+    remove(comp) {
+        delete this.components[comp];
+    }
+    has(comp) {
+        return Object.keys(this.components).includes(comp);
+    }
+}
+
+class Component {
+    constructor(name) {
+        this.name = name;
+        this.game = null;
+        this.entity = null;
+    }
+    update() {}
+    draw() {}
+}
+
+class Transform extends Component {
+    constructor(cx, cy, vx = 0, vy = 0) {
+        super('transform');
+        this.cx = cx; this.cy = cy;
+        this.vx = vx; this.vy = vy;
+    }
+    update() {
+        this.cx += this.vx;
+        this.cy += this.vy;
+    }
+}
+
+class AABB extends Component {
+    constructor(w, h) {
+        super('aabb');
+        this.w = w; this.h = h;
+    }
+    get x() { return this.entity.get('transform').cx - this.w/2 }
+    get y() { return this.entity.get('transform').cy - this.h/2 }
+    intersects(other) {
+        return other.x <= this.x + this.w
+            && other.x + other.w >= this.x 
+            && other.y <= this.y + this.h
+            && other.y + other.h >= this.y;
+    }
+}
+
+class BoxShape extends Component {
+    constructor(color) { 
+        super('boxshape');
+        this.color = color;
+    }
+    draw() {
+        var aabb = this.entity.get('aabb');
+        this.game.drawRect(this.color, aabb.x, aabb.y, aabb.w, aabb.h);
+    }
+}
+
+class BallShape extends Component {
+    constructor(color) { 
+        super('ballshape');
+        this.color = color;
+    }
+    draw() {
+        var transform = this.entity.get('transform');
+        var w = this.entity.get('aabb').w;
+        this.game.drawBall(this.color, transform.cx, transform.cy, w/2);
+        // TODO: this should be code for creating an ellipse... height could be different from width
+    }
+}
+
 class Game {
     constructor(canvasID) {
         var canvas = document.getElementById(canvasID);
